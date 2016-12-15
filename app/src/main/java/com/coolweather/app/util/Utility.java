3e7babd299e7953c4e7189a798bcc3741d5ad262
unknown <>
2016-12-15 15:19:29 +0800

    Initial commit

---
+++ app/src/main/java/com/coolweather/app/util/Utility.java
@@ -0,0 +1,148 @@
+package com.coolweather.app.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.coolweather.app.db.CoolWeatherDB;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class Utility {
+
+	/**
+	 * 解析和处理服务器返回的省级数据
+	 */
+	public synchronized static boolean handleProvincesResponse(
+			CoolWeatherDB coolWeatherDB, String response) {
+		if (!TextUtils.isEmpty(response)) {
+			try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i < allProvinces.length(); i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getString("id"));
+                    // 将解析出来的数据存储到Province表
+					coolWeatherDB.saveProvince(province);
+			     }
+                return true;
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 解析和处理服务器返回的市级数据
+	 */
+	public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int provinceId) {
+		if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getString("id"));
+                    city.setProvinceId(provinceId);
+                 // 将解析出来的数据存储到City表
+					coolWeatherDB.saveCity(city);
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+		}
+		return false;
+	}
+
+	/**
+	 * 解析和处理服务器返回的县级数据
+	 */
+	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0; i < allCounties.length(); i++) {
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+					// 将解析出来的数据存储到County表
+					coolWeatherDB.saveCounty(county);
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+		}
+		return false;
+	}
+
+	/**
+	 * 解析服务器返回的JSON数据，并将解析出的数据存储到本地。
+	 */
+	public static void handleWeatherResponse(Context context, String response) {
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+            JSONArray HeWeather5 = jsonObject.getJSONArray("HeWeather5");
+            JSONObject basic = (JSONObject) HeWeather5.get(0);
+            
+            JSONArray dailyForecast = jsonObject.getJSONArray("daily_forecast");
+            JSONObject cond = dailyForecast.getJSONObject(1);
+            JSONObject tmp = (JSONObject) dailyForecast.get(7);
+            
+			String cityName = basic.getString("city");	
+			String weatherCode = basic.getString("id");
+			
+			String temp1 = tmp.getString("temp1");
+			String temp2 = tmp.getString("temp2");
+			
+			String weatherDesp = cond.getString("txt_d");
+			
+			String publishTime = basic.getJSONObject("update").getString("date");
+			
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+					weatherDesp, publishTime);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 将服务器返回的所有天气信息存储到SharedPreferences文件中。
+	 */
+	public static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weatherDesp,
+			String publishTime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager
+				.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
+	}
+
+}
